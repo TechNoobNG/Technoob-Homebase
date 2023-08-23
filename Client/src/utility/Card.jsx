95f836a9b8d94ef5dca60d60765e2320e5aa30fe
FrankLucky1 <franklucky0@gmail.com>
2023-08-23 17:05:50 +0100

    resources ui fixes

--- Client/src/utility/Card.jsx
+++ Client/src/utility/Card.jsx
@@ -13,26 +13,31 @@ const Card = ({ photo, titleText, subTitleText, pText, link, removeItem, id, use
 
     return (
         <div
-            className="relative w-full sm:w-[230px] h-[120px] sm:h-[320px] sm:mt-10 p-3 flex flex-col justify-start items-center rounded-2xl shadow-md"
+            className="relative w-[160px] sm:w-[260px] h-[300px] sm:h-[370px] sm:mt-10 flex flex-col rounded-2xl shadow-md"
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
         >
-            <a className="flex flex-col h-full w-full sm:w-full sm:h-[full] justify-start items-center">
-                <div className="relative flex flex-col justify-center items-center w-full h-full sm:h-[280px] mb-2">
-                    <img src={photo} alt={titleText} className="w-[90%] h-[100%] object-contain p-2" />
+            <a className="flex flex-col justify-center items-center h-full w-full sm:w-full sm:h-full">
+                <div className="flex flex-col justify-center items-center w-[200px] h-[200px] sm:w-[250px] sm:h-[300px] ">
+                    <div className='flex flex-col justify-center  items-center w-[120px] sm:w-[200px] h-[320px] sm:h-[230px] mb-2'>
+                        <img src={photo} alt={titleText} className="w-full h-full object-cover p-1 bg-blue-500" />
+
+                    </div>
+                    
                     <div className="sm:flex flex-col gap-2 text-center justify-center items-center">
-                        <h1 className="text-xs text-base md:text-xl font-bold">{titleText}</h1>
+                        <h1 className="text-xs  md:text-base font-bold">{titleText}</h1>
                         <div className="hidden sm:flex items-center">
-                            <span className="rounded-full bg-blue-500 text-white px-2 py-1 text-xs">
+                            <span className="rounded-full bg-blue-500 text-white px-2 text-xs">
                                 {subTitleText.toUpperCase()}
                             </span>
                         </div>
                     </div>
                     {isHovered && (
-                        <div className="absolute inset-0.5px flex justify-center items-center">
-                            <div className="flex flex-col gap-1">
+                        <div className="absolute w-full h-full flex justify-center items-center">
+                            <div className=' w-full h-full bg-gray-400 opacity-30 rounded-2xl'/>
+                            <div className="absolute flex flex-col gap-1">
                                 <a href={link}>
-                                    <button className="bg-blue-500 text-xs md:text-xl w-[100%] text-white px-2 py-1 rounded">
+                                    <button className=" bg-blue-500 text-xs md:text-lg w-[100%] text-white px-2 py-1 rounded">
                                         View
                                     </button>
                                 </a>
