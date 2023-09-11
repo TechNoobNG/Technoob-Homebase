de62d8abfaf90144eba63233ddffdfc9caef616c
Oluwatobiloba Aremu <oluwatobiloba.f.a@gmail.com>
2023-09-11 15:41:28 +0100

    rr

--- Function-Worker/src/functions/scheduleNoobJobs.js
+++ Function-Worker/src/functions/scheduleNoobJobs.js
@@ -1,25 +1,26 @@
 const { app } = require('@azure/functions');
-const { deleteExpiredJobs } = require("../../Jobs/index")
-const honeybadger = require('../../../Server/utils/honeybadger');
 
 console.log("running")
 app.timer('scheduleNoobJobs', {
     schedule: '0 */1 * * * *',
     handler: async (myTimer, context) => {
         context.log('Timer function processed request.');
-        honeybadger.notify({
-            name: "Triggered Bi-daily Job",
-            message: myTimer
-       })
+        console.log("Timer function processed request.")
         try {
-           await deleteExpiredJobs(context);
+           // const { deleteExpiredJobs } = require("../../Jobs/index")
+            // const honeybadger = require('../../../Server/utils/honeybadger');
+            // honeybadger.notify({
+            //         name: "Triggered Bi-daily Job",
+            //         message: myTimer
+            // })
+          // await deleteExpiredJobs(context);
         } catch (err) {
             console.log(err)
             context.error(err)
-            honeybadger.notify({
-                name: "Failed To Trigger Bi-daily Job",
-                message: err
-           })
+        //     honeybadger.notify({
+        //         name: "Failed To Trigger Bi-daily Job",
+        //         message: err
+        //    })
         }
     }
 });
