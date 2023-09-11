ee5419025bd933779f14cc2d60cc3f90b8b542b6
Oluwatobiloba Aremu <oluwatobiloba.f.a@gmail.com>
2023-09-11 14:33:14 +0100

    up

--- Function-Worker/src/functions/scheduleNoobJobs.js
+++ Function-Worker/src/functions/scheduleNoobJobs.js
@@ -3,11 +3,7 @@ const { deleteExpiredJobs } = require("../../Jobs/index")
 const honeybadger = require('../../../Server/utils/honeybadger');
 
 app.timer('scheduleNoobJobs', {
-<<<<<<< HEAD
     schedule: '0 */1 * * * *',
-=======
-    schedule: '0 * */12 * * *',
->>>>>>> 3cd18c18cc6419bb478c6f9db5319c22b4478b19
     handler: async (myTimer, context) => {
         context.log('Timer function processed request.');
         honeybadger.notify({
