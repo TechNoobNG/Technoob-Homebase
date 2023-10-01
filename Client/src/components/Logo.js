85d4435595935952e51bdd2c898dab4b33d2fc77
Jasmin1002 <jasmineolofin@gmail.com>
2023-10-01 20:38:55 +0100

    refactor: Make the application Logo dedicate comp

---
+++ Client/src/components/Logo.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Logo() {
+  return (
+    <div>
+      <Link to="/" className="text-2xl font-extrabold text-[#5E7CE8]">
+        Tech Noob
+      </Link>
+    </div>
+  );
+}
+
+export default Logo;
