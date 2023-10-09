55a0ba36ceecc6b99aea5aa2c1d573d9eddda59a
Jasmin1002 <jasmineolofin@gmail.com>
2023-10-09 16:39:40 +0100

    feat: Setup navigation routes for each page

--- Client/src/user-dashboard/App.js
+++ Client/src/user-dashboard/App.js
@@ -1,9 +1,24 @@
-import AppLayout from "./Layout/AppLayout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import AppLayout from "./layout/AppLayout";
+import UserDashboard from "./components/UserDashboard";
+import Resources from "./components/Resources";
+import UserEvents from "./components/UserEvents";
+import Quiz from "./components/Quiz";
 
 function App() {
   return (
     <div className="app">
-      <AppLayout />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route path="/user" element={<UserDashboard />} />
+            <Route path="/resources" element={<Resources />} />
+            <Route path="/event" element={<UserEvents />} />
+            <Route path="/quiz" element={<Quiz />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
