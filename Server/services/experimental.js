3256ae36302359ac7b5646ed77224cd880b039fc
Oluwatobiloba Aremu <oluwatobiloba.f.a@gmail.com>
2023-10-09 20:06:50 +0100

    retry

--- Server/services/experimental.js
+++ Server/services/experimental.js
@@ -1,15 +1,12 @@
-const workerpool = require('workerpool');
+const pool = require("../experimental/index")
 
-
-// const pool = require("../experimental/index")
-
-// module.exports = {
-//     compressFile: async function (file) {
-//         try {
-//             const result = await pool.exec('compress',[file])
-//             return result
-//         } catch (error) {
-//             console.log(error)
-//         }
-//     }
-// }
+module.exports = {
+    compressFile: async function (file) {
+        try {
+            const result = await pool.exec('compress',[file])
+            return result
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
