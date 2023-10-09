e60d3a427572c5c172b82033adb6baf9d914c6b7
Jasmin1002 <jasmineolofin@gmail.com>
2023-10-09 14:02:40 +0100

    feat: Add App file for user-dashboard

---
+++ Client/user-dashboard/App.js
@@ -0,0 +1,12 @@
+import AppLayout from "./Layout/AppLayout";
+
+function App() {
+  return (
+    <div className="app">
+      <AppLayout />
+      {/* <aside className="bg-black w-80 h-screen fixed">&nbsp;</aside> */}
+    </div>
+  );
+}
+
+export default App;
