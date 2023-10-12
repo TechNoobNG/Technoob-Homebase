7e9b683d348a9d452df5d3c6d3b501082989750e
Jasmin1002 <jasmineolofin@gmail.com>
2023-10-12 23:46:14 +0100

    feat: Compose and extract Avatar component

---
+++ Client/src/user-dashboard/components/AvatarProfile.js
@@ -0,0 +1,17 @@
+import Avatar from "./Avatar";
+
+function AvatarProfile() {
+  return (
+    <figure>
+      <Avatar />
+      <figcaption>
+        <p>Hello Adeola,</p>
+        <p>
+          Today is Monday, <time>15th May, 2023</time>
+        </p>
+      </figcaption>
+    </figure>
+  );
+}
+
+export default AvatarProfile;
