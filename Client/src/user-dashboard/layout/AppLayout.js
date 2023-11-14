cf6f3eeca4df15de18cc53e899f58375b9546e87
Jasmin1002 <jasmineolofin@gmail.com>
2023-11-14 17:30:41 +0100

    refactor: Nest children witin usercontext provider

--- Client/src/user-dashboard/layout/AppLayout.js
+++ Client/src/user-dashboard/layout/AppLayout.js
@@ -1,16 +1,17 @@
 import Aside from "../components/Aside";
 import Header from "../components/Header";
 import MainContent from "../components/MainContent";
+import { UserProvider } from "../context/userContext";
 
 function AppLayout() {
   return (
-    <>
+    <UserProvider>
       <Header />
       <div className="content">
         <Aside />
         <MainContent />
       </div>
-    </>
+    </UserProvider>
   );
 }
 
