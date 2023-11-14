7bbb42deab9d80401bf2eb04c75fbe62383ff70f
Jasmin1002 <jasmineolofin@gmail.com>
2023-11-14 17:05:17 +0100

    feat: Import styles for user dashboard

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import {AppProvider} from './AppContext/AppContext';
-import App from './App';
-import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "./dev";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "./user-dashboard/index.css";
+import { AppProvider } from "./AppContext/AppContext";
+import App from "./App";
+import { DevSupport } from "@react-buddy/ide-toolbox";
+import { ComponentPreviews, useInitial } from "./dev";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <AppProvider>
-            <DevSupport ComponentPreviews={ComponentPreviews}
-                        useInitialHook={useInitial}
-            >
-                <App/>
-            </DevSupport>
-        </AppProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <AppProvider>
+      <DevSupport
+        ComponentPreviews={ComponentPreviews}
+        useInitialHook={useInitial}
+      >
+        <App />
+      </DevSupport>
+    </AppProvider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
