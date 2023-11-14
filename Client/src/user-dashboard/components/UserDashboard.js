03098a86d71702f9fff18090ed3139fab0411267
Jasmin1002 <jasmineolofin@gmail.com>
2023-11-14 17:27:05 +0100

    refactor: Import and render Error component onload

--- Client/src/user-dashboard/components/UserDashboard.js
+++ Client/src/user-dashboard/components/UserDashboard.js
@@ -1,15 +1,25 @@
+import { useContext } from "react";
 import ActivityOverview from "./ActivityOverview";
 import AvatarProfile from "./AvatarProfile";
 import Banner from "./Banner";
 import JobEvent from "./JobEvent";
+import { UserContext } from "../context/userContext";
+import Loader from "../animation/Loader";
 
 function UserDashboard() {
+  const { isLoading } = useContext(UserContext);
   return (
     <>
-      <AvatarProfile />
-      <Banner />
-      <ActivityOverview />
-      <JobEvent />
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <AvatarProfile />
+          <Banner />
+          <ActivityOverview />
+          <JobEvent />
+        </>
+      )}
     </>
   );
 }
