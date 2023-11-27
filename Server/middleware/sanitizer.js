9e20e71323c22b072241d5b449a0ee5dce6fb88d
Oluwatobiloba Aremu <oluwatobiloba.f.a@gmail.com>
2023-11-27 21:30:53 +0100

    update validator

--- Server/middleware/sanitizer.js
+++ Server/middleware/sanitizer.js
@@ -1,7 +1,5 @@
-
 const xss = require('xss');
 
-//  list of safe HTML tags and attributes
 const xssOptions = {
   whiteList: {
     a: ['href', 'title', 'target'],
@@ -18,31 +16,105 @@ const xssOptions = {
     h5: [],
     h6: [],
     br: [],
-    hr: []
-  }
+    hr: [],
+  },
 };
 
 function sanitize(req, res, next) {
-    sanitizeObject(req.body);
-    sanitizeObject(req.params);
-    sanitizeObject(req.query);
-    next();
+
+  req.body = preventNoSQLInjection(req.body, "body");
+  sanitizeObject(req.body);
+
+  req.params = preventNoSQLInjection(req.params, "params");
+  sanitizeObject(req.params);
+
+  req.query = preventNoSQLInjection(req.query, "query");
+  sanitizeObject(req.query);zXkik
+
+  next();
 }
 
 function sanitizeObject(obj) {
-    for (const key in obj) {
-        if (typeof obj[key] === 'object') {
-            if (Array.isArray(obj[key])) {
-                obj[key].forEach((element, index) => {
-                    obj[key][index] = xss(element, xssOptions);
-                });
-            } else {
-                sanitizeObject(obj[key]);
-            }
-        } else {
+  for (const key in obj) {
+    if (typeof obj[key] === 'object') {
+      if (Array.isArray(obj[key])) {
+        obj[key] = obj[key].map((element) => xss(element, xssOptions));
+      } else {
+        sanitizeObject(obj[key]);
+      }
+    } else {
+        if (typeof obj[key] !== 'number') {
             obj[key] = xss(obj[key], xssOptions);
         }
     }
+  }
 }
+
+
+function preventNoSQLInjection(obj, type) {
+  if (type === 'params' || type === 'query') {
+    convertValues(obj);
+  } else if (type === "body") {
+    obj = sanitizeJson(obj);
+  }
+    return obj
+}
+
+function convertValues(obj) {
+  for (const key in obj) {
+    if (typeof obj[key] === 'object' && obj[key] !== null) {
+      obj[key] = obj[key].toString();
+    } else if (typeof obj[key] === 'string') {
+      const intValue = parseInt(obj[key], 10);
+      if (!isNaN(intValue)) {
+        obj[key] = intValue;
+      }
+    } else if (typeof obj[key] === 'number') {
+      const intValue = Number.isInteger(obj[key]) ? parseInt(obj[key], 10) : obj[key];
+      obj[key] = intValue;
+    } else if (typeof obj[key] === 'boolean') {
+      obj[key] = obj[key];
+    }
+  }
+}
+
+// function sanitizeString(input) {
+//     console.log(sanitizedValue)
+//     if (typeof input === 'string') {
+//       return input.replace(/[$.]/g, '');
+//     } else if (Array.isArray(input)) {
+//       return input.map(item => sanitizeString(item));
+//     } else if (typeof input === 'object') {
+//       return sanitizeNoSQLInjection(input);
+//     } else {
+//       return input;
+//     }
+//   }
+
+function isMongoOperator(key) {
+    return key.startsWith('$') && key !== '$';
+  }
+  
+function sanitizeJson(input) {
+  if (typeof input === 'object') {
+    if (Array.isArray(input)) {
+      return input.map(sanitizeJson);
+    } else {
+      const sanitizedObject = {};
+      for (const key in input) {
+        if (Object.prototype.hasOwnProperty.call(input, key)) {
+          const sanitizedKey = isMongoOperator(key) ? key.replace(/\$/g, '') : key;
+          sanitizedObject[sanitizedKey] = sanitizeJson(input[key]);
+        }
+      }
+      return sanitizedObject;
+    }
+  } else if (typeof input === 'string') {
+    return isMongoOperator(input) ? input.replace(/\$/g, '') : input;
+  } else {
+    return input;
+  }
+}
+
 
 module.exports = sanitize;
