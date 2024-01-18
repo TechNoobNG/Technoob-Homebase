1e06def14cfa402374b044dddf5523df2ba8a7c9
Oluwatobiloba Aremu <oluwatobiloba.f.a@gmail.com>
2024-01-18 18:45:20 +0100

    fx

--- Server/utils/automations/scraper.js
+++ Server/utils/automations/scraper.js
@@ -64,7 +64,7 @@ module.exports = {
         ],
         readTimeout: 5 * 60 * 1000,
         headless: 'new',
-        targetFilter: (target) => !!target.url,
+        // targetFilter: (target) => !!target.url,
       });
       //let page = (await browser.pages())[0];
       let page = await browser.newPage();
@@ -72,7 +72,7 @@ module.exports = {
 
       await page.goto('https://ng.indeed.com', { waitUntil: 'load' });
 
-      await page.waitForTimeout(5000)
+      await page.waitForTimeout(6000)
 
       // // try to solve the cloudflare captcha 
       // await page.click('body')
