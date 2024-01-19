e0417f883d70233c215e56fbf75a2b6afd0734e1
Oluwatobiloba Aremu <oluwatobiloba.f.a@gmail.com>
2024-01-19 23:31:28 +0100

    update user-agent

--- Server/utils/automations/scraper.js
+++ Server/utils/automations/scraper.js
@@ -197,7 +197,7 @@ module.exports = {
           try {
             const response = await axios.get(indeedUrl, {
                 headers: {
-                  "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.0.0 Safari/537.36'
+                  "User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.2.1 Safari/605.1.15'
                 }
             });
             const parser = new XMLParser();
