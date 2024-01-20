cbde4c878f43322a9b58771c24be610d08d5cf31
Oluwatobiloba Aremu <oluwatobiloba.f.a@gmail.com>
2024-01-20 14:46:31 +0100

    ops

---
+++ Function-Worker-2.0/scrubNoobJobsTrigger/index.js
@@ -0,0 +1,20 @@
+const config = require('../utils/config')['production'];
+const q = config.SCRAPER_QUANTITY;
+const posted = config.SCRAPER_OLDEST_JOB_FETCH || 4;
+const expires = config.SCRAPED_JOBS_EXPIRES;
+
+const { scrapeJobs,scrapeJobsV2 } = require("../Jobs/index")
+
+module.exports = async function (context, req) {
+   let responseMessage = `Triggered Succesfully`
+    try {
+        await scrapeJobsV2();
+        await scrapeJobs(q, posted, expires, context);
+    } catch (error) {
+        responseMessage = `Triggered Failed`
+    }
+    context.res = {
+        status: 200,
+        body: responseMessage
+    };
+}
