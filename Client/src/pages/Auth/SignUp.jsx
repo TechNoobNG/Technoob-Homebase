e5b450ece363417a5d929e17ca97c1000ef768d0
Oluwatobiloba <ooluwatobiiloba@gmail.com>
2024-03-15 19:21:41 +0100

    Revert "Ft/impact"

--- Client/src/pages/Auth/SignUp.jsx
+++ Client/src/pages/Auth/SignUp.jsx
@@ -51,11 +51,11 @@ if(form.Password !== form.ConfirmPassword) alert('Passwords do not match')
 else{
   setLoading(true)
       try {
-        const postUser = await fetch("https://api.technoob.tech/api/v1/authenticate/register", requestOptions)
-
+        const postUser = await serverApi.post('authenticate/register', raw)
         const cookies = postUser.headers.get('Set-Cookie');
         if (cookies) {
           const cookie = cookies.split(';')[0].split('=')[1];
+          console.log("cookie",cookie)
           localStorage.setItem('connect-sid', cookie);
           setIsLoggedIn(true);
           navigate('/Dashboard')
@@ -67,6 +67,7 @@ else{
         if(result.status === 'success') navigate('/');
 
       } catch (error) {
+        console.log('this is the error from register',error)
         setIsLoggedIn(false)
       }finally{
         setLoading(false);
